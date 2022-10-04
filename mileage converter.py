10c18f12399cd7e344acd2abdab6e1b7c6596ddf
quin-Amazin <ugochiamazin@gmail.com>
2022-10-04 21:07:29 +0100

    mile converter

--- mileage converter.py
+++ mileage converter.py
@@ -2,5 +2,6 @@ print("How many kilometers did you run today?")
 kms = input("Enter kilometers here: ")
 miles = float(kms)/1.60934
 miles = round(miles, 2)
-print("Your {}km run was 50.5{}miles".format(kms,miles))
+
+print("Your {}km run was {}miles".format(kms,miles))
 
